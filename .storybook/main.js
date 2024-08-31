3a7ec7674d92dfa483e5396eb27b5afe889011de
rowanna <rowanna@rowannaui-MacBookAir.local>
2024-09-01 05:57:09 +0900

    study: 공부한 내용 정리

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,6 +4,9 @@ const config = {
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
+    // Form 요소에 값 입력 시뮬레이션
+    // 버튼을 클릭 -> 대화 상자를 열거나 닫음
+    // 목록 항목을 드래그하여 순서륿 바꾸는 동작을 자동화
     '@storybook/addon-interactions',
     'storybook-addon-react-router-v6',
   ],
